3b6096673678fdfa10bc5b4079b6ebc1c2b02892
Kohei Yamamoto <kh2yam1@gmial.com>
2019-08-27 17:38:42 -0400

    Implement detail

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -28,21 +28,61 @@ export class Launch extends Component {
     return (
       <Fragment>
         <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
-          {
-            ({ loading, error, data }) => {
-              if(loading) return <h4>Loading...</h4>;
-              if(error) console.log(error);
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
 
-              console.log(data);
-              // const { mission_name, flight_number, launch_year, launch_success, rocket: { rocket_id, rocket_name, rocket_type }} = data.launch;
+            const {
+              mission_name,
+              flight_number,
+              launch_year,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type }
+            } = data.launch;
 
-              return <div>test</div>;
+            return (
+              <div>
+                <h1 className="display-4 my-3">
+                  <span className="text-dark">Mission:</span> {mission_name}
+                </h1>
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Successful:{' '}
+                    <span
+                      className={classNames({
+                        'text-success': launch_success,
+                        'text-danger': !launch_success
+                      })}
+                    >
+                      {launch_success ? 'Yes' : 'No'}
+                    </span>
+                  </li>
+                </ul>
 
-              // return (
-              //   <h1 className="display-4 my-3"><span className="text-dark">Mission:</span> {mission_name}</h1>
-              // );
-            }
-          }
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">
+                    Rocket Name: {rocket_name}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket Type: {rocket_type}
+                  </li>
+                </ul>
+                <hr />
+                <Link to="/" className="btn btn-secondary">
+                  Back
+                </Link>
+              </div>
+            );
+          }}
         </Query>
       </Fragment>
     )
