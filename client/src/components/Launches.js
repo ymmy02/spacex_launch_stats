3ba914cb428d96f4397840bee221c7e612b1a51e
Kohei Yamamoto <kh2yam1@gmial.com>
2019-08-27 16:30:23 -0400

    Implement list

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
     launches {
       flight_number
       mission_name
-      launch_data_local
+      launch_date_local
       launch_success
     }
   }
@@ -16,20 +17,26 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
           {
             ({ loading, error, data }) => {
-              if(loading) return <h4>Loading...</h4>
+              if(loading) return <h4>Loading...</h4>;
               if(error) console.log(error);
-              console.log(data);
-
-              return <h1>test</h1>
+              return (
+                <Fragment>
+                  {
+                    data.launches.map(launch => {
+                      return <LaunchItem key={launch.flight_number} launch={launch} />;
+                    })
+                  }
+                </Fragment>
+              );
             }
           }
         </Query>
-      </div>
+      </Fragment>
     )
   }
 }
